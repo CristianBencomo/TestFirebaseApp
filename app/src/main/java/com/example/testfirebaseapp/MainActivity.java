50d1fc51f3c38d742ca9473cac526db98830b6e2
CristianBencomo <cristan.bencomo@outlook.com>
2021-01-26 13:03:26 -0500

    Bugged version/code backup

--- app/src/main/java/com/example/testfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/testfirebaseapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.testfirebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,47 +11,55 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
 
-    //DatabaseReference fDatabase = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mDatabase;
-    Button Submit_btn;
-    EditText Name_field, Car_field, Location_field, Fuel_field;
-    String name, car, location, fuel;
-    String tag = "LOGGING";
+public class MainActivity extends AppCompatActivity
+{
+    private Button sendBtn, rtvBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_retrieve);
 
-        Name_field = findViewById(R.id.Name_field);
-        Car_field = findViewById(R.id.Car_field);
-        Location_field = findViewById(R.id.Location_field);
-        Fuel_field = findViewById(R.id.Fuel_field);
-        Submit_btn = (Button) findViewById(R.id.Submit_btn);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        sendBtn = findViewById(R.id.Send_btn);
+        rtvBtn = findViewById(R.id.Retrieve_btn);
 
-        Submit_btn.setOnClickListener(new View.OnClickListener() {
+        sendBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                name = Name_field.getText().toString();
-                car = Car_field.getText().toString();
-                location = Location_field.getText().toString();
-                fuel = Fuel_field.getText().toString();
-                Log.d(tag , "The value of the variable Name at this point is:" + name);
-
-                mDatabase.child(name).child("Car").setValue(car);
-                mDatabase.child(name).child("Location").setValue(location);
-                mDatabase.child(name).child("Fuel Level (%)").setValue(fuel);
-                Toast.makeText(getApplicationContext(), "Your data has been recorded by our servers", Toast.LENGTH_SHORT).show();
-                //mDatabase.child("User_01").child("Lucas").child("Car Model").setValue("Corolla");
-                //mDatabase.child("User_01").child("Lucas").child("Birthday").setValue("April 16");
+            public void onClick(View v)
+            {
+                openSendActivity();
             }
         });
+
+        rtvBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openRetrieveActivity();
+            }
+        });
+    }
+
+    public void openSendActivity()
+    {
+        Intent intent = new Intent(this, SendActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRetrieveActivity()
+    {
+        Intent intent = new Intent(this, RetrieveActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/testfirebaseapp/SendActivity.java
@@ -0,0 +1,78 @@
+package com.example.testfirebaseapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class SendActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private Button Submit_btn;
+    private EditText Name_field, Car_field, Location_field, Fuel_field;
+    private String name, car, location, fuel;
+    //private String tag = "LOGGING";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name_field = findViewById(R.id.Name_field);
+        Car_field = findViewById(R.id.Car_field);
+        Location_field = findViewById(R.id.Location_field);
+        Fuel_field = findViewById(R.id.Fuel_field);
+        Submit_btn = (Button) findViewById(R.id.Submit_btn);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Submit_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                sendData();
+            }
+        });
+    }
+
+    public void sendData()
+    {
+        HashMap<String, String> dataMap = new HashMap<String, String>();
+
+        name = Name_field.getText().toString().trim();
+        car = Car_field.getText().toString().trim();
+        location = Location_field.getText().toString().trim();
+        fuel = Fuel_field.getText().toString().trim();
+
+        dataMap.put("Car", car);
+        dataMap.put("Location", location);
+        dataMap.put("Fuel (%)", fuel);
+
+        mDatabase.child(name).setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(), "Your data has been recorded by our server", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error... Could not store data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
